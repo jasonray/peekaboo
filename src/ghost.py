7a801407b09656387493e117f03a3bf7a4f1c7ff
Jason Ray <jayray.net@gmail.com>
2023-11-05 08:26:19 -0500

    updates to ghost process
    Signed-off-by: Jason Ray <jayray.net@gmail.com>

--- src/ghost.py
+++ src/ghost.py
@@ -1,46 +1,113 @@
 import time
 import signal
 import os
-
+import argparse
+import datetime
 
 def main():
-    ghost = Ghost(60)
+    # Initialize parser
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("-t", "--ttl", dest="ttl", default=0, help = "Specifies the amount of time (in seconds) to keep ghost alive")
+    parser.add_argument("-s", "--sleep", dest="sleep_duration", default=1, help = "How often to check for closing process")
+    args = parser.parse_args()
+ 
+    ghost = Ghost(args.ttl, args.sleep_duration)
     ghost.start()
 
 class Ghost:
-    alive_time = None
-    shutdown_requested = False
+    _ttl = None 
+    _sleep_duration = None
+    _shutdown_requested = False
+    _start_time = None 
 
-    def __init__(self, alive_time_seconds):
-        self.alive_time=alive_time_seconds
-        signal.signal(signal.SIGABRT, self.handler)
+    def __init__(self, ttl, sleep_duration):
+        self.ttl=ttl
+        self.sleep_duration=sleep_duration
+        signal.signal(signal.SIGABRT, self.signal_handler)
+
+    @property
+    def ttl(self):
+        return self._ttl
+    
+    @ttl.setter
+    def ttl(self, value):
+        self._ttl = int(value)
+
+    @property
+    def sleep_duration(self):
+        return self._sleep_duration
+    
+    @sleep_duration.setter
+    def sleep_duration(self, value):
+        self._sleep_duration = int(value)
+
+    @property
+    def start_time(self):
+        return self._start_time
+    
+    @start_time.setter
+    def start_time(self, value):
+        self._start_time = value
+
+    @property
+    def shutdown_requested(self):
+        return self._shutdown_requested
+    
+    @shutdown_requested.setter
+    def shutdown_requested(self, value):
+        self._shutdown_requested = bool(value)
+
+    @property
+    def uptime(self):
+        now_time = time.time()
+        delta = now_time - self.start_time
+        return delta
+
+    def is_expired(self):
+        if self.ttl == 0:
+            return False
+        else:
+            return self.uptime>self.ttl
+        
+    @property
+    def pid(self):
+        return os.getpid()
 
     def start(self):
-        print("starting ghost")
-        print("- alive_time", self.alive_time)
-        print("pid", os.getpid() )
+        self.log("starting ghost")
+        self.log("- ttl", self.ttl)
+        self.log("- sleep_duration", self.sleep_duration)
+        self.log("pid", self.pid)
 
         continue_process=True
-        start_time = time.time()
-        print("start time", start_time)
+        self.start_time = time.time()
+        self.log("start time", self.start_time)
         while continue_process:
-            now_time = time.time()
-            delta = round(now_time - start_time,2)
-            print(f"[delta:{delta}]")
-            if delta>self.alive_time:
+            self.log(f"[uptime:{round(self.uptime,1)}]")
+            if self.shutdown_requested:
+                self.log('shutdown request recognized')
+                continue_process=False
+            elif self.is_expired():
+                self.log('expired')
                 continue_process=False
             else:
-                time.sleep(1)
-            if self.shutdown_requested:
-                print('shutdown request recognized')
-                continue_process=False
-        print("complete")
+                time.sleep(self.sleep_duration)
+        self.log(f"process complete [uptime={round(self.uptime,1)}]")
 
-    def handler(self, signum, frame):
-        print(f'received signal [{signum}]')
+    def signal_handler(self, signum, frame):
+        self.log(f'received signal [{signum}]')
         if signum == 6:
             self.shutdown_requested = True
-            print('shutdown requested', self.shutdown_requested)
+            self.log('shutdown requested')
+
+    def log(self, *messages):
+        message_buffer=[]
+        for message_entry in messages:
+            message_buffer.append(str(message_entry))
+        message = ' '.join(message_buffer)
+        timestamp = datetime.datetime.now()
+        print(f'{timestamp}\t{self.pid}\t{message}')
 
 if __name__ == "__main__":
     main()
