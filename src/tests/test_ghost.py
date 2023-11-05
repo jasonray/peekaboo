4b3b32c19a67c6c3241ac3fdc17012b450e04e93
Jason Ray <jayray.net@gmail.com>
2023-11-05 09:36:01 -0500

    test ghost.start
    Signed-off-by: Jason Ray <jayray.net@gmail.com>

--- src/tests/test_ghost.py
+++ src/tests/test_ghost.py
@@ -1,5 +1,6 @@
 import unittest
 from src.ghost import Ghost
+import time
 
 
 class TestGhost(unittest.TestCase):
@@ -23,3 +24,16 @@ class TestGhost(unittest.TestCase):
     def test_sleep_duration_default(self):
         ghost = Ghost(ttl=5)
         self.assertEqual(ghost.sleep_duration, 0)
+
+    def test_start(self):
+        ttl = 2
+        sleep_duration = 1
+        precision = 1
+        ghost = Ghost(ttl=ttl, sleep_duration=sleep_duration)
+        start_time = time.time()
+        ghost.start()
+        end_time = time.time()
+        delta = end_time - start_time
+        print(f'[start={round(start_time,1)}][end={round(end_time,1)}][delta={round(delta,1)}]')
+        self.assertTrue(delta >= ttl)
+        self.assertTrue(delta <= ttl + precision)
